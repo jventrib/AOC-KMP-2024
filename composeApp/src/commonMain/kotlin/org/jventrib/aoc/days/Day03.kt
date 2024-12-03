79f267f70c3ca3a48813572fc77915e6057c05f2
jventrib <jventrib@gmail.com>
2024-12-03 07:17:24 +0100

    Day03 Init

--- composeApp/src/commonMain/kotlin/org/jventrib/aoc/days/Day03.kt
+++ composeApp/src/commonMain/kotlin/org/jventrib/aoc/days/Day03.kt
@@ -5,8 +5,12 @@ import org.jventrib.aoc.day
 
 val day03 =
     day<Int>(3) {
-      val inputState = input.map { Report(it, false) }.toMutableStateList()
+      val inputState = input.joinToString()
+          .map { CharStatus(it, false) }.toMutableStateList()
 
-      part1(2, 224) { exec { 0 } }
+      part1(2, 224) { exec {
+        0 } }
       part2(4, 285) { exec { 0 } }
     }
+
+data class CharStatus(val char: Char, val valid: Boolean)
