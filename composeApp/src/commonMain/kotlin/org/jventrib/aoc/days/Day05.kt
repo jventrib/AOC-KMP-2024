0a1c3117ad884513f9e506331d77917aff7066fd
jventrib <jventrib@gmail.com>
2024-12-05 21:08:51 +0100

    Day05 OK

--- composeApp/src/commonMain/kotlin/org/jventrib/aoc/days/Day05.kt
+++ composeApp/src/commonMain/kotlin/org/jventrib/aoc/days/Day05.kt
@@ -4,18 +4,33 @@ import org.jventrib.aoc.day
 
 val day05 =
     day(5) {
+      val after = input.takeWhile { it.isNotBlank() }.map { it.split('|') }
+          .map { it[0] to it[1] }
+          .groupBy { it.first.toInt() }
+          .mapValues { it.value.map { it.second.toInt() } }
 
-      part1(0, 0) {
+      val updates =
+          input.dropWhile { it.isNotBlank() }.drop(1).map { it.split(',').map { it.toInt() } }
+
+      val comparator: (Int, Int) -> Int = { a, b -> if (after[a]?.contains(b) == true) -1 else 0 }
+
+      part1(143, 5268) {
         render {
         }
         exec {
-          0
+          val validUpdates = updates.filter { update ->
+            update == update.sortedWith(comparator)
+          }
+          validUpdates.sumOf { it[it.size / 2] }
         }
       }
-      part2(0, 0) {
+      part2(123, 5799) {
         render { }
         exec {
-          0
+          val validUpdates = updates.filter { update ->
+            update != update.sortedWith(comparator)
+          }.map { it.sortedWith(comparator) }
+          validUpdates.sumOf { it[it.size / 2] }
         }
       }
     }
---
+++ composeApp/src/jvmTest/kotlin/DayTest.kt
@@ -0,0 +1,22 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.funSpec
+import org.jventrib.aoc.Day
+import org.jventrib.aoc.days.day05
+
+class DayTest :
+  FunSpec(
+      {
+        coroutineTestScope = true
+        fun <E> dayPartTest(day: Day<E>) = funSpec {
+          context("Part 1") {
+            test("day part 1 example") { executeAndAssert(day, 1, true) }
+            test("day part 1") { executeAndAssert(day, 1, false) }
+          }
+          context("Part 2") {
+            test("day part 2 example") { executeAndAssert(day, 2, true) }
+            test("day part 2") { executeAndAssert(day, 2, false) }
+          }
+        }
+        include(dayPartTest(day05))
+      },
+  )
